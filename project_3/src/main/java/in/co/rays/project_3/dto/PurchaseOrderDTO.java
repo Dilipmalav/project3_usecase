24a4d79a29bf40710a3497fde5d0d60adaf907b4
YourUserName <dilipmalav8@gmail.com>
2025-02-11 14:14:22 +0530

    done

---
+++ project_3/src/main/java/in/co/rays/project_3/dto/PurchaseOrderDTO.java
@@ -0,0 +1,56 @@
+package in.co.rays.project_3.dto;
+
+import java.util.Date;
+
+public class PurchaseOrderDTO extends BaseDTO {
+
+	private int totalQuantity;
+	private String product;
+	private Date orderDate;
+	private double totalCost;
+
+	public int getTotalQuantity() {
+		return totalQuantity;
+	}
+
+	public void setTotalQuantity(int totalQuantity) {
+		this.totalQuantity = totalQuantity;
+	}
+
+	public String getProduct() {
+		return product;
+	}
+
+	public void setProduct(String product) {
+		this.product = product;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(double totalCost) {
+		this.totalCost = totalCost;
+	}
+
+	@Override
+	public String getKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
